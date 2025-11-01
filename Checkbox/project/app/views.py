dcd6c26ecc2ac5245f41a7fa46bc7bccb71574b6
Pratima kushwa <pratimakushwaha479@gmail.com>
2025-11-01 23:34:22 +0530

    first commit

---
+++ Checkbox/project/app/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+def landing(request):
+    return  render(request,'landing.html')
+
+def select1(request):
+    print("hello")
+
+
+    print(request.method)
+    print(request.GET)
+
+    x=request.GET.get('hobby')
+    print(x)
